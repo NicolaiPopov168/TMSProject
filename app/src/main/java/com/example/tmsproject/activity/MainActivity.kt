c65e8f9aa4727f971446eea0dafb18b1ef176fd0
Nicolai_Popov <71674594+NicolaiPopov168@users.noreply.github.com>
2022-05-17 02:52:06 +0300

    BottomNavigationView

--- app/src/main/java/com/example/tmsproject/activity/MainActivity.kt
+++ app/src/main/java/com/example/tmsproject/activity/MainActivity.kt
@@ -2,11 +2,38 @@ package com.example.tmsproject.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.tmsproject.R
+import com.example.tmsproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setBottomNavListener()
+        setContentView(binding.root)
+    }
+
+    private fun setBottomNavListener() {
+        binding.bNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.settings -> {
+                    Log.d("MyLog", "Settings")
+                }
+                R.id.notes -> {
+                    Log.d("MyLog", "Notes")
+                }
+                R.id.shop_list -> {
+                    Log.d("MyLog", "List")
+                }
+                R.id.new_item -> {
+                    Log.d("MyLog", "New")
+                }
+            }
+            true
+        }
     }
 }
